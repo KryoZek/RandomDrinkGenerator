a7eb111fa6bf2f0e35673d85eb26412e5983894c
kryozek <kryozek@protonmail.com>
2022-11-15 21:52:42 -0400

    Optimized loop, while -> for

--- main.py
+++ main.py
@@ -3,41 +3,28 @@ import random as rd
 import sys
 
 
-
 def main():
-
-    setFlag = False
-
-    if len(sys.argv)-1 == 1:
-        drink_amount = int(sys.argv[1]) - 1
-        setFlag = True
-
-    drinks = []
-    toppings = []
 
     with open("/home/kryozek/Reliquary/Codebase/Projects/RandDrinkGen/drinks.txt", 'r') as f:
         drinks = [line.rstrip() for line in f]
-
     f.close()
 
     with open("/home/kryozek/Reliquary/Codebase/Projects/RandDrinkGen/toppings.txt", 'r') as f:
         toppings = [line.rstrip() for line in f]
-
     f.close()
 
-    ctr = 0
-    while len(drinks) > 0:
+    drink_amount = len(drinks)
+
+    if len(sys.argv)-1 == 1:
+        drink_amount = int(sys.argv[1])
+
+    for i in range(drink_amount):
         randDrink   =   drinks.pop(rd.randrange(len(drinks)))
         randTopping =   rd.choice(toppings)
 
         drink = randDrink + "\nTopping: " + randTopping
-        ctr += 1
+        print(f"\nDrink #{i+1}\n{drink}")
 
-        print(f"\nDrink #{ctr}\n{drink}")
-
-        if setFlag:
-            if ctr > drink_amount:
-                break
     print("\n")
 
 main()
